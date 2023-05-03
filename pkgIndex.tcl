277151a8179b8304455a8d91ccfb2c277cb84c58
Alex Baker <ambaker1@mtu.edu>
2023-05-02 21:48:47 -0500

    Patch (requires getPID in file)

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.6]} {return}
-package ifneeded mpjobs 0.1 [list source [file join $dir mpjobs.tcl]]
+package ifneeded mpjobs 0.1.1 [list source [file join $dir mpjobs.tcl]]
