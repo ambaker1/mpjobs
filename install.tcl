bda4e44914331027473810c9b793f7704390ed97
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-09-09 09:28:02 -0500

    version

--- install.tcl
+++ install.tcl
@@ -1,4 +1,4 @@
 package require tin 1.0
 tin depend tda 0.1
-set dir [tin mkdir -force mpjobs 0.1.4]
+set dir [tin mkdir -force mpjobs 0.1.3]
 file copy LICENSE README.md pkgIndex.tcl mpjobs.tcl $dir
